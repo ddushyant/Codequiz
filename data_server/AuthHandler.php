9b941ccad26ba7d71e3461a64c3d3ab8dcbea050
Arthur Maciejewicz <arthur.maciejewicz@gmail.com>
2014-07-10 19:12:03 -0400

    Added auth handler for backend

---
+++ data_server/AuthHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+class AuthHandler {
+    public function post() {
+        try {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $rounds = 15;
+            $bcrypt = new Bcrypt($rounds);
+
+            $hash = $bcrypt->hash($password);
+
+            $query = MySQL::getInstance()
+                ->prepare("SELECT 1 FROM codequizuser
+                           WHERE username = :username
+                           AND   password = :password;");
+
+            $query->bindValue(':username', $username, PDO::PARAM_STR);
+            $query->bindValue(':password', $password, PDO::PARAM_STR);
+
+            $query->execute();
+
+            $n = $query->rowCount();
+            echo "MADE THE QUERY!";
+            if ($n) {
+                http_response_code(204);
+            }else {
+                http_response_code(401);
+            }
+        }catch (PDOException $e) {
+            echo "COULD NOT DO SHIT";
+        }
+    }
+}
