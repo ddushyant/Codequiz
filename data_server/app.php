2cefaef1ab965804e63a853fca92d493ff35131a
Arthur Maciejewicz <arthur.maciejewicz@gmail.com>
2014-07-10 02:13:29 -0400

    Updated Routes in data_server router, added user check
    /user/:name now checks if a user exists and should return the
    relevant info. Otherwise, it returns a 404.
    
    Routes updated for user, questions

--- data_server/app.php
+++ data_server/app.php
@@ -1,14 +1,34 @@
 <?php
 
 require('../lib/Toro.php');
+require('ExistHandler.php');
+//require('AuthHandler.php');
+//require('RegistrationHandler.php');
+//require('QuestionCreateHandler.php');
+//require('QuestionGetHandler.php');
+require('sql_helper.php');
+
+try {
+    $db = new PDO(
+        'mysql:host=localhost;dbname=codequiz;charset=utf8', 
+        'codequiz', 
+        'foobar', 
+        array(
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        )
+    );
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
 Toro::serve(array(
-    "/user/exists" => "ExistHandler",
+    "/user/:alpha" => "ExistHandler",
+    "/user/:alpha/exists" => "ExistHandler",
     "/user/auth" => "AuthHandler",
     "/user/register" => "RegistrationHandler",
-    "/question/create" => "QuestionCreateHandler",
-    "/question/get" => "QuestionGetHandler",
-    "/
+    "/question" => "QuestionCreateHandler",
+    "/question/:number" => "QuestionGetHandler",
 ));
 
 ?>
