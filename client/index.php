3d179bf7368bf5ca205d9364956272c747b66464
Jonathan Lysiak <jlysiak84@gmail.com>
2014-07-14 21:21:30 -0400

    Cleaned up form spacing and added radio checkboxes to register

--- client/index.php
+++ client/index.php
@@ -3,7 +3,7 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,7 +33,7 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <![endif]-->
   </head>
 
   <body>
@@ -42,34 +42,38 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
       <form class="form-signin" role="form" name="login" method="post">
         <h3 align="center" >Please Sign In</h3>
 
-        <input type="text" name="user" class="form-control flat" placeholder="UCID or Username" required autofocus>
+        <div class="form-group">
+            <input type="text" name="user" class="form-control flat" placeholder="UCID or Username" required autofocus>
+        </div>
 
-        <input type="password" name="pass" class="form-control flat" placeholder="Password" required>
+        <div class="form-group">
+            <input type="password" name="pass" class="form-control flat" placeholder="Password" required>
+        </div>
 
         <label class="checkbox" for="checkbox1">
-          <input type="checkbox" name="njit" value="true" data-toggle="checkbox" >
+            <input type="checkbox" name="njit" value="true" data-toggle="checkbox" >
             <span style="color: #1ABC9C;">NJIT login?</span>
         </label>
-        <button class="btn btn-embossed btn-primary btn-block" type="submit">Sign in</button>
-        <div id="flash" style="color: white;">
+
+      <button class="btn btn-embossed btn-primary btn-block" type="submit">Sign in</button>
+      <div id="flash" style="color: white;"></div>
       </form>
-
     </div>
-      <div class="row" style="position:absolute; bottom: 20px; width: 80%;">
-        <div class="col-md-4">
-          <a class="btn btn-hg btn-primary" href="register.php">
-            Register Here!
-          </a>
-        </div>
-      </div>
+  <div class="row" style="position:absolute; bottom: 20px; width: 80%;">
+    <div class="col-md-4">
+      <a class="btn btn-hg btn-primary" href="register.php">
+        Register Here!
+    </a>
+</div>
+</div>
 
-    </div> <!-- /container -->
+</div> <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-     <!-- Load JS here for greater good =============================-->
+    <!-- Load JS here for greater good =============================-->
     <script src="js/jquery-1.8.3.min.js"></script>
     <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
     <script src="js/jquery.ui.touch-punch.min.js"></script>
@@ -81,22 +85,22 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
     <script src="js/jquery.tagsinput.js"></script>
     <script src="js/jquery.placeholder.js"></script>
     <script type="text/javascript">
-        $('form').submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-                type: "POST",
-                url: "<?php echo $APP_SERVER_BASE_URL; ?>/auth",
-                data: $('form').serialize(),
-                success: function(data,stat,xhr) {
-                    console.log("Success: ",data);
-                    $('#flash').html(data['message']);
-                },
-                error: function(xhr,stat,err) {
-                    console.log("Fail: ", err);
-                },
-                dataType: "json"
-            });
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "<?php echo $APP_SERVER_BASE_URL; ?>/auth",
+            data: $('form').serialize(),
+            success: function(data,stat,xhr) {
+                console.log("Success: ",data);
+                $('#flash').html(data['message']);
+            },
+            error: function(xhr,stat,err) {
+                console.log("Fail: ", err);
+            },
+            dataType: "json"
         });
+    });
     </script>
-  </body>
+</body>
 </html>
--- client/register.php
+++ client/register.php
@@ -3,7 +3,7 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,39 +33,57 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <![endif]-->
   </head>
 
   <body>
-    
+
     <div class="container">
 
-      <form class="form-signin" role="form" name="login" method="post" >
-        <h3 align="center" >Fill Out This Form</h3>
+        <form class="form-signin" role="form" name="login" method="post" >
+            <h3 align="center" >Fill Out This Form</h3>
 
-        <input type="text" name="user" class="form-control flat" placeholder="Enter Your Cool Username Here..">
-        <input type="password" name="pass" class="form-control flat" placeholder="Password">
-        <input type="password" name="pass" class="form-control flat" placeholder="Confirm Your Password">
+            <div class="form-group">
+                <input type="text" name="user" class="form-control flat" placeholder="Enter Your Cool Username Here..">
+            </div>
+            
+            <div class="form-group">
+                <input type="password" name="pass" class="form-control flat" placeholder="Password">
+            </div>
 
-        <button class="btn btn-embossed btn-primary btn-block" type="submit">Register</button>
-        <div id="flash" style="color: white;"></div>
-      </form>
-      
-      <div class="row" style="position:absolute; bottom: 20px; width: 80%;">
-        <div class="col-md-4">
-          <a class="btn btn-hg btn-primary" href="index.php">
-            Sign In Here!
-          </a>
-        </div>
-      </div>
+            <div class="form-group">
+                <input type="password" name="pass" class="form-control flat" placeholder="Confirm Your Password">
+            </div>
 
-    </div> <!-- /container -->
+            <label class="radio">
+              <input type="radio" name="sorp" value="1" data-toggle="radio">
+              Professor
+            </label>
+
+            <label class="radio">
+              <input type="radio" name="sorp" value="2" data-toggle="radio" checked>
+              Student
+            </label>
+
+          <button class="btn btn-embossed btn-primary btn-block" type="submit">Register</button>
+          <div id="flash" style="color: white;"></div>
+        </form>
+
+  <div class="row" style="position:absolute; bottom: 20px; width: 80%;">
+    <div class="col-md-4">
+      <a class="btn btn-hg btn-primary" href="index.php">
+        Sign In Here!
+    </a>
+</div>
+</div>
+
+</div> <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-     <!-- Load JS here for greater good =============================-->
+    <!-- Load JS here for greater good =============================-->
     <script src="js/jquery-1.8.3.min.js"></script>
     <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
     <script src="js/jquery.ui.touch-punch.min.js"></script>
@@ -77,22 +95,22 @@ $APP_SERVER_BASE_URL = "http://web.njit.edu/~jdl38/application_server/app.php";
     <script src="js/jquery.tagsinput.js"></script>
     <script src="js/jquery.placeholder.js"></script>
     <script type="text/javascript">
-        $('form').submit(function(e) {
-            e.preventDefault();
-            console.log($('form').serialize());
-            $.ajax({
-                type: "POST",
-                url: "<?php echo $APP_SERVER_BASE_URL; ?>/register",
-                data: $('form').serialize(),
-                success: function(data,stat,xhr) {
-                    $('#flash').html(data['message']);
-                },
-                error: function(xhr,stat,err) {
-                    console.log("err", err);
-                },
-                dataType: "json"
-            });
+    $('form').submit(function(e) {
+        e.preventDefault();
+        console.log($('form').serialize());
+        $.ajax({
+            type: "POST",
+            url: "<?php echo $APP_SERVER_BASE_URL; ?>/register",
+            data: $('form').serialize(),
+            success: function(data,stat,xhr) {
+                $('#flash').html(data['message']);
+            },
+            error: function(xhr,stat,err) {
+                console.log("err", err);
+            },
+            dataType: "json"
         });
+    });
     </script>
-  </body>
+</body>
 </html>
