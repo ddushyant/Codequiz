60de389bc8d49149fe244384afc5f5b28b87ead9
Jonathan Lysiak <jlysiak84@gmail.com>
2014-07-16 18:05:43 -0400

    Dynamically load form and get date

--- client/make_question.php
+++ client/make_question.php
@@ -31,30 +31,6 @@
 
 			</div>
 
-			<!-- <div id="torf" style="display: none;">
-				<label class="radio">
-				  <input type="radio" name="group1" value="1" data-toggle="radio" checked>
-				  <span style="color:white">Answer is True</span>
-				</label>
-
-				<label class="radio">
-				  <input type="radio" name="group1" value="2" data-toggle="radio">
-				  <span style="color:white">Answer is False</span>
-				</label>
-			</div>
-
-			<div id="openq" style="display: none;">
-				<div class="form-group">
-		            <input type="text" name="open_answer" class="form-control flat" placeholder="Enter Correct Answer Here" required autofocus>
-		        </div>
-			</div>
-
-			<div id="codeq" style="display: none;">
-				<div class="form-group">
-		            <input type="text" name="code_answer" class="form-control flat" placeholder="Enter the Desired Output" required autofocus>
-		        </div>
-			</div> -->
-
           <button class="btn btn-embossed btn-primary btn-block" type="submit">Submit Question</button>
           <div id="flash" style="color: white;"></div>
         </form>
@@ -76,11 +52,6 @@
   
     <script type="text/javascript">
 
-    	var mult = document.getElementById("mult");
-    	var torf = document.getElementById("torf");
-    	var openq = document.getElementById("openq");
-    	var codeq = document.getElementById("codeq");
-
 	    jQuery("#action-1").click(function(e){
 	    	$("#mult").html("");
 			$("#mult").append('<div class="form-group"><input type="text" name="mult_answer" class="form-control flat" placeholder="Enter Correct Answer Here" required autofocus></div>');
@@ -91,7 +62,10 @@
 
 	    jQuery("#action-2").click(function(e){
 	    	$("#mult").html("");
-	    	$("#mult").append('<label class="radio"><input type="radio" name="group1" value="1" data-toggle="radio" checked><span style="color:white">Answer is True</span></label><label class="radio"><input type="radio" name="group1" value="2" data-toggle="radio"><span style="color:white">Answer is False</span></label>');
+	    	// $("#mult").append('<button type="button" class="btn btn-default">Left</button>');
+                $("#mult").append('<label class="radio"><span class="icons"><span class="first-icon fui-radio-unchecked"></span><span class="second-icon fui-radio-checked"></span></span><input type="radio" name="group1" value="True" data-toggle="radio">Answer is True</label>');
+
+                $("#mult").append('<label class="radio"><span class="icons"><span class="first-icon fui-radio-unchecked"></span><span class="second-icon fui-radio-checked"></span></span><input type="radio" name="group1" value="False" data-toggle="radio" checked>Answer is False</label>');
 		});
 
 		jQuery("#action-3").click(function(e){
@@ -108,19 +82,46 @@
 	 <script type="text/javascript">
     $('form').submit(function(e) {
         e.preventDefault();
-        console.log($('form').serialize());
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:3000/app.php/questionmaker",
-            data: $('form').serialize(),
-            success: function(data,stat,xhr) {
-                $('#flash').html(data['message']);
-            },
-            error: function(xhr,stat,err) {
-                console.log("err", err);
-            },
-            dataType: "json"
+
+
+        var acc = [];
+        var obj = {};
+
+        $("#mult input:not([type='radio'])").each(function(iter) {
+            
+
+            obj = {
+                'answer_key' : iter,
+                'answer_value' : this.value
+            };
+            
+            
+            acc.push(obj);
         });
+
+        var group1 = $("#mult input[name='group1']:checked");
+
+        if (group1.length > 0) {
+            acc.push({
+                "answer_key":"tf",
+                "answer_value":group1.val()
+            });
+        }
+
+        acc = JSON.stringify(acc);
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: "http://localhost:3000/app.php/questionmaker",
+        //     data: $('form').serialize(),
+        //     success: function(data,stat,xhr) {
+        //         $('#flash').html(data['message']);
+        //     },
+        //     error: function(xhr,stat,err) {
+        //         console.log("err", err);
+        //     },
+        //     dataType: "json"
+        // });
     });
     </script>
    
